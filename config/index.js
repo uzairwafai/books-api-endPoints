5064df0506ebda3270bd60875eac8be19b1762e1
Uzair Wafai <uzairwafai000@gmail.com>
2023-11-15 21:34:58 +0530

    env variable fix db connect

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     jwtSecret: process.env.jwtSecret,
     connectionStr: process.env.conStr,
-    // connectionStr:'mongodb+srv://uzairwafai:<password>cluster0.zvn7ya0.mongodb.net/library'
+    // connectionStr:'mongodb+srv://uzairwafai:<password>@cluster0.zvn7ya0.mongodb.net/library'
 };
